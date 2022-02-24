bcaacbd08ebcdb6d216f2e944a80d25348e48571
alumon574 <alumon574@ieselcaminas.org>
2022-02-24 16:34:43 +0100

    test de mayor50 hecho

--- src/com/company/CalculadoraTest.java
+++ src/com/company/CalculadoraTest.java
@@ -1,12 +1,27 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
     @org.junit.jupiter.api.Test
-    void suma(){
-        int resultado=Calculadora.suma(2,3);
-        int esperado= 5;
-        assertEquals(esperado,esperado);
+    void suma() {
+        int resultado = Calculadora.suma(2, 3);
+        int esperado = 5;
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    void resta() {
+        int resultado = Calculadora.resta(8, 3);
+        int esperado = 5;
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    void mayor50() {
+        boolean esperado = Calculadora.mayor50(60);
+        assertTrue(esperado);
     }
 }
